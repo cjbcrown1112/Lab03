adb566f9fd6981faeff23ac52dba0afda1a0446a
Regina Liza Diloy <reginaliza.diloy@gcash.com>
2022-03-11 17:34:50 +0800

    java lab 3

--- Lab3/src/Main.java
+++ Lab3/src/Main.java
@@ -3,6 +3,6 @@ public class Main {
     public static void main(String[] args) {
         NameRepository nameRepository = new NameRepository();
 
-            nameRepository.sendMessage("This is Email is to Remind You to Submit your BIR Form No. 1701");
+            nameRepository.sendMessage("This Email is to Remind You to Submit your BIR Form No. 1701");
         }
     }
